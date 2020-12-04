5dc002a93c3217c5cadcf967fe065ce8c4de739e
LordUnicorn31 <37652360+LordUnicorn31@users.noreply.github.com>
2020-12-04 12:42:55 +0100

    Module animations added

---
+++ Game/Source/Animation.h
@@ -0,0 +1,47 @@
+#include "Defs.h"
+#include "SDL.h"
+#define MAX_FRAMES 60
+
+
+class Animation
+{
+public:
+	bool loop = true;
+	float speed = 1.0f;
+	SDL_Rect frames[MAX_FRAMES];
+
+private:
+	float currentFrame = 0.0f;
+	int lastFrame = 0;
+	int loops = 0;
+
+public:
+
+	void PushBack(const SDL_Rect& collider)
+	{
+		frames[lastFrame++] = collider;
+	}
+
+	SDL_Rect& GetCurrentFrame(float dt)
+	{
+		currentFrame += speed * dt;
+		if (currentFrame >= lastFrame)
+		{
+			currentFrame = (loop) ? 0.0f : lastFrame - 1;
+			loops++;
+		}
+
+		return frames[(int)currentFrame];
+	}
+
+	bool Finished() const
+	{
+		return loops > 0;
+	}
+
+	void Reset()
+	{
+		currentFrame = 0;
+		loops = 0;
+	}
+};
