8443212ab309ea7e8f356838a3077db712d74e53
LordUnicorn31 <37652360+LordUnicorn31@users.noreply.github.com>
2020-12-05 22:25:58 +0100

    enemies animations added (Still not drawn)

--- Game/Source/PatrolEnemy.cpp
+++ Game/Source/PatrolEnemy.cpp
@@ -1,4 +1,6 @@
 #include "PatrolEnemy.h"
+#include "App.h"
+#include "Render.h"
 
 enum class PatrolEnemyState
 {
@@ -12,18 +14,24 @@ PatrolEnemy::PatrolEnemy(iPoint initialPos) : Dynamic(EntityType::PATROL_ENEMY)
 	terminalSpeed = 0.0f;
 	pos = initialPos;
 	initialPosition = initialPos;
+	idleAnimation.PushBack({ 0, 80, 16, 16 });
+	moveAnimation.PushBack({ 16, 80, 16, 16 });
+	moveAnimation.PushBack({ 32, 80, 16, 16 });
+	deathAnimation.PushBack({ 0, 80, 16, 16 });
+	deathAnimation.PushBack({ 80, 80, 16, 16 });
 }
 
 PatrolEnemy::~PatrolEnemy(){}
 
 void PatrolEnemy::Update(float dt)
 {
-
+	//Idle animation by definition
+	currentAnimation = &idleAnimation;
 }
 
 void PatrolEnemy::Draw(float dt)
 {
-
+	app->render->DrawTexture(this->sprite, initialPosition.x, initialPosition.y, &currentAnimation->GetCurrentFrame(dt), 1.0f);
 }
 
 void PatrolEnemy::UpdateLogic()
--- Game/Source/WanderEnemy.cpp
+++ Game/Source/WanderEnemy.cpp
@@ -1,4 +1,6 @@
 #include "WanderEnemy.h"
+#include "App.h"
+#include "Render.h"
 
 enum class WanderEnemyStates
 {
@@ -12,18 +14,23 @@ WanderEnemy::WanderEnemy(iPoint initialPos) : Dynamic(EntityType::WANDER_ENEMY)
 	terminalSpeed = 0.0f;
 	pos = initialPos;
 	initialPosition = initialPos;
+	idleAnimation.PushBack({ 0, 96, 16, 16 });
+	moveAnimation.PushBack({ 16, 96, 16, 16 });
+	moveAnimation.PushBack({ 32, 96, 16, 16 });
+	deathAnimation.PushBack({ 0, 96, 16, 16 });
+	deathAnimation.PushBack({ 80, 96, 16, 16 });
 }
 
 WanderEnemy::~WanderEnemy(){}
 
 void WanderEnemy::Update(float dt)
 {
-
+	currentAnimation = &idleAnimation;
 }
 
 void WanderEnemy::Draw(float dt)
 {
-
+	app->render->DrawTexture(this->sprite, initialPosition.x, initialPosition.y, &currentAnimation->GetCurrentFrame(dt), 1.0f);
 }
 
 void WanderEnemy::UpdateLogic()
